11debb9825e5098b3c4fe84cd945a18dc6cf9603
Brawlence <42910943+Brawlence@users.noreply.github.com>
2020-02-01 14:55:53 +0300

    Completed fetcher
    Fetching and downloading works (in async)

--- phantomjs/download-reddit-posts.js
+++ phantomjs/download-reddit-posts.js
@@ -2,46 +2,63 @@
 
 var fs = require('fs');
 
-//TODO: Fix this to actually fetch all the links
-function dumpLinksTo(outputFileName) {
-	var where_to_look = [	"https://old.reddit.com/user/localroger/submitted/", 
-							"https://old.reddit.com/user/localroger/submitted/?count=25&after=t3_comens", 
-							"https://old.reddit.com/user/localroger/submitted/?count=50&after=t3_aowl0k", 
-							"https://old.reddit.com/user/localroger/submitted/?count=75&after=t3_92owaa",
-							"https://old.reddit.com/user/localroger/submitted/?count=100&after=t3_7wogam"];
-	
-	var links_array = document.querySelectorAll("a[href*='curators'].title");
-	var links_text = '';
-	for (var i = 0; i < links_array.length; i++) {
-		links_text = links_text + links_array[i].href + "\n";
-	};
-	try {
-		fs.write(outputFileName, links_text, 'w');
-		console.log("strings written to " + outputFileName);
-	} catch (error) {
-		console.log(e);
-	}
-}
-
 function loadlinksFrom(file) {
 	var content = fs.read(file);
 	var array = content.split("\n");
 	return array;
-}
+};
 
 function dumpTo(fileName, data, originURL) {
 	data = data.replace(/(http[s]?\:\/\/[\w]{3}\.reddit\.com\/r\/HFY\/comments\/.{6}\/)([\w]+)\//g,'$2.html');
 	data = "<html><head></head><body>" + data + "<div id=\"footer\">SOURCE URL: <a href=\"" + originURL + "\"> " + originURL + " </a></div></body><html>";
 	fs.write(fileName + ".html", data, 'w');
-}
+};
+
+var drp = {
+	report: function(status, url) {
+		if (status !== "success") {
+			return console.log("Unable to process " + url);
+		} else {
+			return console.log("Processed post " + url);
+		}
+	},
+
+	fetchLinks: function (page) {
+		var content = page.evaluate(
+								function(){
+									var links_text = '';
+									var links_array = document.querySelectorAll("a[href*=\'curators\'].title");
+									for (var i = 0; i < links_array.length; i++) {
+										if (links_array[i] !== null) links_text = links_text + links_array[i].href + "\n";
+									};
+									return links_text;
+									}
+								); 
+		try {
+			fs.write("LINKS_LIST.txt", content, 'a');
+			console.log("block of links written");
+		} catch (error) {
+			console.log(e);
+		}
+	},
+
+	grabPostContent: function(page) {
+		var originURL =  page.evaluateJavaScript('function(){return document.URL;}');
+		var HTMLcontent = page.evaluateJavaScript('function(){return document.querySelector(\'div.expando div.usertext-body\').innerHTML;}');
+		var splotted = originURL.split('/');
+		var name = splotted[splotted.length-2];
+		dumpTo(name, HTMLcontent, originURL);			
+	}
+};
 
 /*
-Saves given urls to files
+Processes given array of URLs by a given method
 @param array of URLs to render
+@param actionsToDo Function called on every hop, dictates what to do on each page
 @param callbackPerUrl Function called after finishing each URL, including the last URL
 @param callbackFinal Function called after finishing everything
 */
-function downloadPostToFiles (urls, callbackPerUrl, callbackFinal) {
+function processListOfURLS (urls, actionsToDo, callbackPerUrl, callbackFinal) {
     var next, page, retrieve, webpage;
     webpage = require("webpage");
     page = null;
@@ -59,14 +76,9 @@ function downloadPostToFiles (urls, callbackPerUrl, callbackFinal) {
             return page.open(url, function(status) {
                 if (status === "success") {
                     return window.setTimeout((function() {
-                        var originURL =  page.evaluateJavaScript('function(){return document.URL;}');
-						var HTMLcontent = page.evaluateJavaScript('function(){return document.querySelector(\'div.expando div.usertext-body\').innerHTML;}');
-						var splotted = originURL.split('/');
-						var name = splotted[splotted.length-2];
-						dumpTo(name, HTMLcontent, originURL);			
-
+						actionsToDo(page);
                         return next(status, url);
-                    }), 3000);
+                    }), 1000);
                 } else {
                     return next(status, url);
                 }
@@ -78,17 +90,14 @@ function downloadPostToFiles (urls, callbackPerUrl, callbackFinal) {
     return retrieve();
 };
 
-//dumpLinksTo("LINKS_LIST.txt");
+var where_to_look = [	"http://old.reddit.com/user/localroger/submitted/", 
+						"http://old.reddit.com/user/localroger/submitted/?count=25&after=t3_comens", 
+						"http://old.reddit.com/user/localroger/submitted/?count=50&after=t3_aowl0k", 
+						"http://old.reddit.com/user/localroger/submitted/?count=75&after=t3_92owaa",
+						"http://old.reddit.com/user/localroger/submitted/?count=100&after=t3_7wogam"];
+
+processListOfURLS(where_to_look, drp.fetchLinks, drp.report, function() {return console.log("completed link fetching");});
+
 var URL_array = loadlinksFrom("LINKS_LIST.txt");
 
-downloadPostToFiles(
-	URL_array,
-	(function(status, url) {
-		if (status !== "success") {
-			return console.log("Unable to dump '" + url + "'");
-		} else {
-			return console.log("Dumped post " + url);
-		}
-	}),
-	function() {return phantom.exit();}
-);
+//processListOfURLS(URL_array, drp.grabPostContent, drp.report, function() {return phantom.exit();});
