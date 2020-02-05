a0b760d1a88c27ea67e7dedc8774338b35e5f86a
Brawlence <42910943+Brawlence@users.noreply.github.com>
2020-02-05 19:35:58 +0300

    Added footer links, added Book 4 part 2, index generator

--- phantomjs/download-reddit-posts.js
+++ phantomjs/download-reddit-posts.js
@@ -3,15 +3,16 @@
 var fs = require('fs');
 
 function loadlinksFrom(file) {
+	console.log(file + " loaded")
 	var content = fs.read(file);
 	var array = content.split("\n");
 	return array;
 };
 
-function dumpTo(fileName, data, originURL) {
+function dumpTo(fileName, data, nav, originURL) {
 	var template = fs.read("template.html");
 	data = data.replace(/(http[s]?\:\/\/[\w]{3}\.reddit\.com\/r\/HFY\/comments\/.{6}\/)([\w]+)\//g,'$2.html');
-	data = template.replace(/REPLACE-THIS-PART-WITH-CONTENT/g, data + "<div id=\"footer\">SOURCE URL: <a href=\"" + originURL + "\"> " + originURL + " </a></div>");
+	data = template.replace(/REPLACE-THIS-PART-WITH-CONTENT/g, data + nav + " <hr> <div id=\"footer\"> <a style=\"float:left;\"href=\"index.html\">[ INDEX ]</a> <a style=\"float:right;\"href=\"" + originURL + "\">[ SOURCE post on HFY ]</a></div>");
 	fs.write("..\\" + fileName + ".html", data, 'w');
 };
 
@@ -46,9 +47,10 @@ var drp = {
 	grabPostContent: function(page) {
 		var originURL =  page.evaluateJavaScript('function(){return document.URL;}');
 		var HTMLcontent = page.evaluateJavaScript('function(){return document.querySelector(\'div.expando div.usertext-body\').innerHTML;}');
+		var navParagraph = page.evaluateJavaScript('function(){return document.querySelector(\'div.expando div.usertext-body p a\').parentElement.innerHTML}');
 		var splotted = originURL.split('/');
 		var name = splotted[splotted.length-2];
-		dumpTo(name, HTMLcontent, originURL);			
+		dumpTo(name, HTMLcontent, navParagraph, originURL);			
 	}
 };
 
