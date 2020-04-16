631dda610f37e47f8265887ce05e915dda689daa
Brawlence <42910943+Brawlence@users.noreply.github.com>
2020-04-16 09:53:17 +0300

    Новый генератор Индекса
    (скрипт make_index для PhantomJS)
    Рефакторинг, комментарии, удобная локализация текста, не зависящий от языка (и ветки гита) шаблон template.html, автоматическое определение переведённых глав и добавление к другим метки "перевод ещё не готов"

--- phantomjs/make_index.js
+++ phantomjs/make_index.js
@@ -1,22 +1,69 @@
 "use strict";
 
-var fs = require('fs');
-var content = fs.read('LINKS_LIST.txt');
-console.log("File loaded");
-var pepega = content.split("\n");
-var template = fs.read("template.html");
-var data = "<H1> Кураторы </H1> <hr />\n\t<h2>Книга 1</h2>\n\t";
-var book = 2;
-for (var i_2 = pepega.length-1; i_2 >= 0; i_2--) { 
-		var splotted = pepega[i_2].split('/');
-		var name = splotted[splotted.length-2];
-        name = name.replace(/(oc_)?(the_)?curators_/g, '');
-        if (name.indexOf("book_"+book) > -1) data = data + "\n\t<h2>Книга " + book++ +"</h2>\n\t";
-		data = data + " <a href=\"" + pepega[i_2].replace(/[\r\n\t]/g, '') + "\">" + name.replace(/book_[\d]_/g, '') + "</a>  ";
-        data = data.replace(/   /g, ' • ');
-		console.log("added " + i_2);
+// localization strings
+var	loc_HTML_lang = "ru",
+	loc_HTML_desc = "Фанатский перевод научно-фантастической саги The Curators Роджера Вильямса",
+	loc_title = "Кураторы",
+	loc_book = "Книга",
+	loc_part = "Часть",
+	loc_missing = "Перевод ещё не готов.",
+	loc_desc = "Научно-фантастическая повесть в жанре HFY от",
+	loc_authorName = "Роджера Уильямса";
+
+// add new seclast() method
+if (!Array.prototype.seclast){
+	Array.prototype.seclast = function(){
+		return this[this.length - 2];
+	};
 };
-data = data.replace(/(http[s]?\:\/\/[\w]{3}\.reddit\.com\/r\/HFY\/comments\/.{6}\/)([\w]+)\//g,'$2.html');
-data = template.replace(/REPLACE-THIS-PART-WITH-CONTENT/g, data + "<hr /> Научно-фантастическая повесть в жанре HFY от <a href=\"https://reddit.com/u/localroger/\">Роджера Уильямса</a>");
+
+// phantomJS — prepare the filesystem submodule
+var fs = require('fs');
+
+// read and localize the template
+var template = fs.read("template.html"); 
+template = template.replace(/\$lang/g, loc_HTML_lang);
+template = template.replace(/\$title/g, loc_title);
+template = template.replace(/\$description/g, loc_HTML_desc);
+
+// read and split the links list
+var links = fs.read('LINKS_LIST.txt');
+var links_array = links.split("\n");
+console.log("Template & Links-list loaded");
+
+var data = "\t<H1>" + loc_title + "</H1>\n<hr />\n\t<h2>" + loc_book + " 1</h2>\n\t";
+var book = 2;
+
+for (var i_2 = links_array.length-1; i_2 >= 0; i_2--) { // phantomJS — somewhy for-of loop won't work
+		var link = links_array[i_2];
+		var name = link.split('/').seclast();
+
+		// add a subdivision if next book is encountered
+		if (name.indexOf("book_"+book) > -1) data = data + "\n\t<h2>" + loc_book + " " + book++ +"</h2>\n\t";
+
+		// clean and localize the name
+		name = name.replace(/(oc_)?(the_)?curators_(book_[\d]_)?/g, '');
+		name = name.replace(/part_([\d][\d]?)/g, loc_part + ' $1');
+
+		// clean and convert the link to local file instead of reddit post
+		link = link.replace(/[\r\n\t]/g, '');
+		link = link.replace(/(http[s]?\:\/\/[\w]{3}\.reddit\.com\/r\/HFY\/comments\/.{6}\/)([\w]+)\//g,'$2.html');
+		
+		// add present local links, omit missing from index
+		if (fs.exists("../" + link)) {
+			data = data + "<a href=\"" + link + "\">" + name + "</a>  ";
+			console.log("added " + link);
+		} else {
+			data = data + "<a class=\"notyet\" title=\"" + loc_missing + "\" href=\"index.html\">" + name + "</a>  ";
+			console.log("omitted " + link);
+		}
+	
+		// add nice dots between chapters (parts)
+		data = data.replace(/  \</g, ' • <'); 
+};
+
+data = data + "<hr />" + loc_desc + " <a href=\"https://reddit.com/u/localroger/\">" + loc_authorName + "</a>";
+data = template.replace(/\$content/g, data);
+
 fs.write("../index.html", data, 'a');
 phantom.exit();
