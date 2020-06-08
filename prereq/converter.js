300ff8c608ad56e48a1117165a0b65550bdeafd9
Brawlence <42910943+Brawlence@users.noreply.github.com>
2020-06-08 14:22:17 +0300

    Обновил converter.js
    При наведении на проценты показываются символы кириллицы\латиницы,
    при близости к 95% показываются пропущенные английские символы.
    Вынес параграфы навигации и нижний колонтитул в константы.

--- prereq/converter.js
+++ prereq/converter.js
@@ -1,7 +1,10 @@
 "use strict";
 
-const DIV_STYLE = "top: 5%; left: 5%; height: 90%; width: 90%; position: fixed; z-index: 255; text-align: left; font-family: 'Open Sans', sans-serif; font-size: 16px;";
-const TEXTAREA_STYLE = "border-width: 3px; border-style: solid; border-color: black; width: 100%; height: 100%;";
+const STYLE_DIV = "top: 5%; left: 5%; height: 90%; width: 90%; position: fixed; z-index: 255;";
+const STYLE_P = "text-align: left; font-family: 'Open Sans', sans-serif; font-size: 16px; margin: 3px";
+const STYLE_MD = "border-width: 3px; border-style: solid; border-color: black; width: 100%; height: 100%;";
+const navParagraph = "[Первый эпизод] • [Предыдущий] • Следующий  ";
+const linksFooter = "***\n\n[Зеркало на GitHub] • [Оригинал на r/HFY]\n\n[Зеркало на GitHub]: https://brawlence.github.io/Localroger-The-Curators/\n[Оригинал на r/HFY]:";
 
 function createMDControl() {
     let mdDiv = document.body.appendChild(document.createElement('div'));
@@ -10,9 +13,10 @@ function createMDControl() {
 };
 
 function copyMarkdownText() {
-	if (document.getElementById('elderMagicField') === null) {
+	if (document.getElementById('elderMagicDiv') === null) {
         let text = document.getElementsByClassName("md")[0].innerHTML;
         
+        text = text.replace(/&nbsp;/g, ' ');
         text = text.replace(/\<p\>/g, '');
         text = text.replace(/\<\/p\>/g, '  ');
         text = text.replace(/\<(\/)?em\>/g, '*');
@@ -20,20 +24,29 @@ function copyMarkdownText() {
         text = text.replace(/\<\/span\>/g, '**');
 
         let ruAmount = text.replace(/[^ЁёА-я]/g, '').length;
-        let enAmount = text.replace(/[^A-z]/g, '').length;
+        let enAmount = text.replace(/[^A-Za-z]/g, '').length;
         let completionPerc = (ruAmount*100/(enAmount+ruAmount)).toFixed(1);
+        let missed = "";
+        if ((completionPerc > 95) && (enAmount > 0) && (enAmount < 50)) {
+            missed = text.replace(/[^A-Za-z ]/g, '').replace(/[\s]+/g, ' ');
+            missed = `\nПропущенные символы: ${missed}`;
+        };
 
-        text = "[Первый эпизод] • [Предыдущий] • Следующий  \n\n" + text + "[Первый эпизод] • [Предыдущий] • Следующий  \n\n***\n\n[Зеркало на GitHub] • [Оригинал на r/HFY]\n\n[Зеркало на GitHub]: https://brawlence.github.io/Localroger-The-Curators/\n[Оригинал на r/HFY]:\n\n[Первый эпизод]: \n[Предыдущий]: \n";
+        text = navParagraph + text + "\n"
+             + navParagraph + "\n\n" + linksFooter + "\n\n[Первый эпизод]: \n[Предыдущий]: \n";
 
         const elderMagicDiv = document.body.appendChild(document.createElement('div'));
             elderMagicDiv.id = "elderMagicDiv";
-			elderMagicDiv.style = DIV_STYLE;
-            elderMagicDiv.textContent = "Прогресс перевода: " + completionPerc + "%; кириллических символов: " + ruAmount + ", латинских символов: " +  enAmount;
+			elderMagicDiv.style = STYLE_DIV;
 
-		const elderMagicField = elderMagicDiv.appendChild(document.createElement('textarea'));
-			elderMagicField.id = "elderMagicField";
-			elderMagicField.style = TEXTAREA_STYLE;
-			elderMagicField.value = text;
+        const percentageDisplay = elderMagicDiv.appendChild(document.createElement('p'));
+            percentageDisplay.textContent = `Перевод: ${completionPerc}%`;
+            percentageDisplay.title = `Кириллица:\t${ruAmount}\nЛатиница:\t${enAmount}` + missed;
+            percentageDisplay.style = STYLE_P;
+
+		const markdownText = elderMagicDiv.appendChild(document.createElement('textarea'));
+			markdownText.style = STYLE_MD;
+			markdownText.value = text;
 	} else {
 		document.getElementById('elderMagicDiv').parentElement.removeChild(document.getElementById('elderMagicDiv'));
 	}
